97e07f5e4bd5d9257837c694613414a1d2a04167
Jay C <jaychap77@yahoo.com>
2020-11-25 08:38:04 -0500

    update readme and screenshot

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,14 @@
-// mySQL
-const mysql = require("mysql");
 
+const mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-<<<<<<< HEAD
-    password: "Jason#5908",
-=======
     password: "root",
->>>>>>> 4ae7992cfcdb332ae1dbac078abd108486f08163
     database: "burgers_db"
   });
 }
-
-
-// Connection
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for ORM
+connection.connect();
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    id INT NOT NULL AUTO INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(40) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
