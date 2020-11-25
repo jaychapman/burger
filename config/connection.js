ec66384f40e77676c5d9c04466f4dce52c26901d
Jay C <jaychap77@yahoo.com>
2020-11-25 07:31:58 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Jason#5908",
     database: "burgers_db"
   });
 }
